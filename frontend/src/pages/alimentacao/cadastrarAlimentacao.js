894c857da0907c41f6718428a478deaf1ed69375
silviafds <silviafernandes974@gmail.com>
2023-07-08 22:05:25 -0300

    feat: implementação de checkbx

--- frontend/src/pages/alimentacao/cadastrarAlimentacao.js
+++ frontend/src/pages/alimentacao/cadastrarAlimentacao.js
@@ -1,23 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../../styles/css/alimentacaoBovino.css';
 import Menu from '../../components/menu';
 
 const CadastrarAlimentacao = () => {
-
     const [nomeForm, setNomeForm] = useState('');
     const [faltaConsumirForm, setFaltaConsumirForm] = useState('');
     const [qntDiariaRecomendadaForm, setQntDiariaRecomendadaForm] = useState('');
-
+    const [ingredientes, setIngredientes] = useState([]);
+    const [ingredientesSelecionados, setIngredientesSelecionados] = useState([]);
     const navigate = useNavigate();
 
-    const handleSubmitForm = async e => {
+    useEffect(() => {
+        // Recupera os dados do armazenamento local
+        const dadosIngredientes = localStorage.getItem('dadosIngredientes');
+        if (dadosIngredientes) {
+            const ingredientesSalvos = JSON.parse(dadosIngredientes);
+            setIngredientes(ingredientesSalvos);
+        }
+    }, []);
+
+    const handleSubmitForm = async (e) => {
         e.preventDefault();
         const payload = {
             nome: nomeForm,
             faltaConsumir: faltaConsumirForm,
-            qntDiariaRecomendada: qntDiariaRecomendadaForm
+            qntDiariaRecomendada: qntDiariaRecomendadaForm,
+            ingredientesSelecionados: ingredientesSelecionados
         };
+        // Resto do código...
+    };
+
+    const handleSelecionarIngrediente = (ingrediente) => {
+        if (ingredientesSelecionados.includes(ingrediente)) {
+            setIngredientesSelecionados(ingredientesSelecionados.filter(item => item !== ingrediente));
+        } else {
+            setIngredientesSelecionados([...ingredientesSelecionados, ingrediente]);
+        }
     };
 
     return (
@@ -27,33 +46,73 @@ const CadastrarAlimentacao = () => {
             <div className="formularioAlimentacao" style={{ marginBottom: "10%" }}>
                 <form className="formulario" onSubmit={e => { handleSubmitForm(e) }}>
                     <div className="sub-div">
-                        <div className="id_"><p>Nome</p></div>
-                        <input type="text" className="nomeAlimentacao" required onChange={e => setNomeForm(e.target.value)} />
+                        <div className="id_">
+                            <p>Nome</p>
+                        </div>
+                        <input
+                            type="text"
+                            className="nomeAlimentacao"
+                            required
+                            value={nomeForm}
+                            onChange={e => setNomeForm(e.target.value)}
+                        />
 
-                        <div className="id_"><p>Falta consumir</p></div>
-                        <input type="text" className="faltaConsumirAlimentacao" required onChange={e => setFaltaConsumirForm(e.target.value)} />
+                        <div className="id_">
+                            <p>Falta consumir</p>
+                        </div>
+                        <input
+                            type="text"
+                            className="faltaConsumirAlimentacao"
+                            required
+                            value={faltaConsumirForm}
+                            onChange={e => setFaltaConsumirForm(e.target.value)}
+                        />
 
-                        <div className="id_"><p>Quantidade diária recomendada</p></div>
-                        <input type="text" className="qntDiariaRecomendadaAlimentacao" required onChange={e => setQntDiariaRecomendadaForm(e.target.value)} />
+                        <div className="id_">
+                            <p>Quantidade diária recomendada</p>
+                        </div>
+                        <input
+                            type="text"
+                            className="qntDiariaRecomendadaAlimentacao"
+                            required
+                            value={qntDiariaRecomendadaForm}
+                            onChange={e => setQntDiariaRecomendadaForm(e.target.value)}
+                        />
 
+                        <div className="id_">
+                            <p>Ingredientes</p>
+                        </div>
+                        <div className='checkboxContainer'>
+                            {ingredientes.map(ingrediente => (
+                                <div key={ingrediente.nome} className='checkboxItem'>
+                                    <label>
+                                        <input
+                                            type="checkbox"
+                                            value={ingrediente.nome}
+                                            checked={ingredientesSelecionados.includes(ingrediente.nome)}
+                                            onChange={() => handleSelecionarIngrediente(ingrediente.nome)}
+                                        />
+                                        {ingrediente.nome}
+                                    </label>
+                                </div>
+                            ))}
+                        </div>
                         <div>
-                            <button variant="warning" type="submit" value="submit" className="btn btn-success"
-                                style={{ backgroundColor: "#83A93A", borderColor: "#6D3B00", margin: "30px 30px 0 0" }}>
-                                Cadastre-se
-                            </button>
-                            <button className="btn btn-success"
-                                style={{ backgroundColor: "#6D3B00", borderColor: "#6D3B00", marginTop: "30px" }} variant="warning"
-                                onClick={e => navigate('/login')}>
-                                Login
+                            <button
+                                variant="warning"
+                                type="submit"
+                                value="submit"
+                                className="btn btn-success"
+                                style={{ backgroundColor: "#83A93A", borderColor: "#6D3B00", margin: "30px 30px 0 0" }}
+                            >
+                                Cadastrar
                             </button>
                         </div>
                     </div>
                 </form>
             </div>
-
         </div>
     );
-
 };
 
 export default CadastrarAlimentacao;
--- frontend/src/pages/ingredientes/cadastrarIngrediente.js
+++ frontend/src/pages/ingredientes/cadastrarIngrediente.js
@@ -18,7 +18,40 @@ const CadastrarIngrediente = () => {
             qtdEstoque: qtdEstoqueForm,
             unidade: unidadeForm
         };
+
+        // Função para salvar os dados no armazenamento local
+        salvarDados(payload);
     };
+
+    const salvarDados = (dados) => {
+        // Obtém os dados existentes do armazenamento local (se houver)
+        const dadosExistentes = localStorage.getItem('dadosIngredientes');
+
+        let novosDados = [];
+
+        if (dadosExistentes) {
+            // Se já houver dados salvos, converte para um array
+            novosDados = JSON.parse(dadosExistentes);
+        }
+
+        // Adiciona os novos dados ao array
+        novosDados.push(dados);
+
+        // Salva o array atualizado no armazenamento local
+        localStorage.setItem('dadosIngredientes', JSON.stringify(novosDados));
+
+        // Limpa os campos do formulário
+        setNomeForm('');
+        setQtdEstoqueForm('');
+        setUnidadeForm('');
+
+        // Exibe os dados salvos no console
+        console.log('Dados salvos:', novosDados);
+
+        // Navega para a página desejada após cadastrar os dados
+        navigate('/');
+    };
+
 
     return (
         <div>
@@ -31,7 +64,7 @@ const CadastrarIngrediente = () => {
                         <input type="text" className="nomeIngrediente" required onChange={e => setNomeForm(e.target.value)} />
 
                         <div className="id_"><p>Quantidade Estoque</p></div>
-                        <input type="email" className="qtdEstoqueIngrediente" required onChange={e => setQtdEstoqueForm(e.target.value)} />
+                        <input type="text" className="qtdEstoqueIngrediente" required onChange={e => setQtdEstoqueForm(e.target.value)} />
 
                         <div className="id_"><p>Unidade</p></div>
                         <input type="text" className="unidadeIngrediente" required onChange={e => setUnidadeForm(e.target.value)} />
@@ -39,12 +72,7 @@ const CadastrarIngrediente = () => {
                         <div>
                             <button variant="warning" type="submit" value="submit" className="btn btn-success"
                                 style={{ backgroundColor: "#83A93A", borderColor: "#6D3B00", margin: "30px 30px 0 0" }}>
-                                Cadastre-se
-                            </button>
-                            <button className="btn btn-success"
-                                style={{ backgroundColor: "#6D3B00", borderColor: "#6D3B00", marginTop: "30px" }} variant="warning"
-                                onClick={e => navigate('/login')}>
-                                Login
+                                Cadastrar
                             </button>
                         </div>
                     </div>
