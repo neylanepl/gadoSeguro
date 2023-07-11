48370cb3ef18829ff49a0694c4a749b2f38dc232
Danilo <danilo25aciole@gmail.com>
2023-07-11 17:51:32 -0300

    Carteira/id/doses

--- backend/src/api/controllers/CarteiraController.js
+++ backend/src/api/controllers/CarteiraController.js
@@ -44,6 +44,18 @@ class CarteiraController {
             })
         }
     }
+    //Retornar as doses de uma Carteira pelo ID 
+    async show_registros_carteiraId(request, response) {
+        const { id } = request.params
+        try {
+            const carteira = await CarteiraService.getCarteiraDosesBovino(id)
+            return response.status(200).json(carteira)
+        } catch (err) {
+            return response.status(400).json({
+                error: err
+            })
+        }
+    }
 
     //Deletar Carteira pelo ID
     async delete_carteira(request, response) {
--- backend/src/api/routes/carteirasRoutes.js
+++ backend/src/api/routes/carteirasRoutes.js
@@ -5,6 +5,7 @@ const CarteiraController = require('../controllers/CarteiraController')
 router.post('/carteira', CarteiraController.create_carteira)
 router.get('/carteira', CarteiraController.show_carteiras)
 router.get('/carteira/:id', CarteiraController.show_carteiraId)
+router.get('/carteira/:id/doses', CarteiraController.show_registros_carteiraId)
 router.delete('/carteira/:idBovino', CarteiraController.delete_carteira)
 
 module.exports = router
