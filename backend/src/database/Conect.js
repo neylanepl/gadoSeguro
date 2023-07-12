c436028e109b01c0fa562e6bd1be7fc7fad096e2
Raíssa Santos <raissa.santos.095@ufrn.edu.br>
2023-07-12 02:57:07 -0300

    fix: url certa

--- backend/src/database/Conect.js
+++ backend/src/database/Conect.js
@@ -4,7 +4,7 @@ async function connect() {
   if (global.connection && global.connection.state !== 'disconnected')
     return global.connection
   //const connection = await mysql.createConnection("mysql://{usuario}:{senha}@localhost:3306/GadoSeguro");
-  const connection = await mysql.createConnection("mysql://root:2002@localhost:3306/GadoSeguro");
+  const connection = await mysql.createConnection("mysql://teste:Acesso_1@localhost:3306/GadoSeguro");
   global.connection = connection;
   return connection;
 }
