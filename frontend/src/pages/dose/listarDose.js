1c2b449d25fa6b823b665a2a1b1ec0efa2043a95
Neylane <neylanepl@gmail.com>
2023-07-18 07:48:39 -0300

    fix: estilização

--- frontend/src/pages/dose/listarDose.js
+++ frontend/src/pages/dose/listarDose.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Menu from '../../components/menu';
-import '../../styles/css/global.css';
 
 import gadoSeguro from '../../services/connectionGadoSeguro';
 
